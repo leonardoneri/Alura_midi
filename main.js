d001fbd5c444a0bbf304041be0f130916be94583
Leonardo Neri <leonardo.sneri@stone.com.br>
2022-10-29 15:38:37 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()
-}
+function tocaSom(idElementoAudio){
+    const elemento = document.querySelector(idElementoAudio);
 
+    if(elemento   && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log('Elemento não encontrado ou seletor invalido')
+    }
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas [0].onclick = tocaSomPom
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;//template string
+
+    listaDeTeclas [contador].onclick = function(){
+        tocaSom(idAudio) ;
+    };
+
+    tecla.onkeydown = function(evento){
+
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
 
